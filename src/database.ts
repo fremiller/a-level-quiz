1a917865cc4bfc4d4e998fad0b64509e2f291f4f
fishfred <10870360+fishfred@users.noreply.github.com>
2019-03-16 12:59:54 +0000

    Changed Mongo DB URL to env variable

--- src/database.ts
+++ src/database.ts
@@ -69,7 +69,8 @@ export class Database extends Module {
 
         // Connect to the database
         console.log("\tConnecting")
-        mongoose.connect("mongodb://localhost:27017/quiztest", {
+        console.log(process.env.MONGO_URI)
+        mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true
         });
 
